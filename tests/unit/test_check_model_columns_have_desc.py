7f09424222ea684f75382368e5f519f9e185632e
dangkv <vo.dangkh@gmail.com>
2022-11-21 16:46:44 -0500

    feat: add config tests in pytest

--- tests/unit/test_check_model_columns_have_desc.py
+++ tests/unit/test_check_model_columns_have_desc.py
@@ -3,23 +3,33 @@ import pytest
 from dbt_gloss.check_model_columns_have_desc import check_column_desc
 from dbt_gloss.check_model_columns_have_desc import main
 
-# Input args, valid manifest, expected return value
+# Input args, valid manifest, valid_config, expected return value
 TESTS = (
-    (["aa/bb/with_column_description.sql",'--is_test'], True, 0),
-    (["aa/bb/with_column_description.sql",'--is_test'], False, 1),
-    (["aa/bb/without_columns_description.sql",'--is_test'], True, 1),
-    (["aa/bb/with_some_column_description.sql",'--is_test'], True, 1),
+    (["aa/bb/with_column_description.sql", "--is_test"], True, True, 0),
+    (["aa/bb/with_column_description.sql", "--is_test"], False, True, 1),
+    (["aa/bb/without_columns_description.sql", "--is_test"], True, True, 1),
+    (["aa/bb/with_some_column_description.sql", "--is_test"], True, True, 1),
+    (["aa/bb/with_column_description.sql", "--is_test"], True, False, 0),
 )
 
 
 @pytest.mark.parametrize(
-    ("input_args", "valid_manifest", "expected_status_code"), TESTS
+    ("input_args", "valid_manifest", "valid_config", "expected_status_code"), TESTS
 )
 def test_check_model_columns_have_desc(
-    input_args, valid_manifest, expected_status_code, manifest_path_str
+    input_args,
+    valid_manifest,
+    valid_config,
+    expected_status_code,
+    manifest_path_str,
+    config_path_str,
 ):
     if valid_manifest:
         input_args.extend(["--manifest", manifest_path_str])
+
+    if valid_config:
+        input_args.extend(["--config", config_path_str])
+
     status_code = main(input_args)
     assert status_code == expected_status_code
 
--- tests/unit/test_check_model_has_meta_keys.py
+++ tests/unit/test_check_model_has_meta_keys.py
@@ -2,22 +2,37 @@ import pytest
 
 from dbt_gloss.check_model_has_meta_keys import main
 
-# Input args, valid manifest, expected return value
+# Input args, valid manifest, valid_config, expected return value
 TESTS = (
-    (["aa/bb/with_meta.sql",'--is_test', "--meta-keys", "foo", "bar"], True, 0),
-    (["aa/bb/with_meta.sql",'--is_test', "--meta-keys", "foo", "bar"], False, 1),
-    (["aa/bb/without_meta.sql",'--is_test', "--meta-keys", "foo", "bar"], True, 1),
+    (["aa/bb/with_meta.sql", "--is_test", "--meta-keys", "foo", "bar"], True, True, 0),
+    (["aa/bb/with_meta.sql", "--is_test", "--meta-keys", "foo", "bar"], False, True, 1),
+    (
+        ["aa/bb/without_meta.sql", "--is_test", "--meta-keys", "foo", "bar"],
+        True,
+        True,
+        1,
+    ),
+    (["aa/bb/with_meta.sql", "--is_test", "--meta-keys", "foo", "bar"], True, False, 0),
 )
 
 
 @pytest.mark.parametrize(
-    ("input_args", "valid_manifest", "expected_status_code"), TESTS
+    ("input_args", "valid_manifest", "valid_config", "expected_status_code"), TESTS
 )
 def test_check_model_meta_keys(
-    input_args, valid_manifest, expected_status_code, manifest_path_str
+    input_args,
+    valid_manifest,
+    valid_config,
+    expected_status_code,
+    manifest_path_str,
+    config_path_str,
 ):
     if valid_manifest:
         input_args.extend(["--manifest", manifest_path_str])
+
+    if valid_config:
+        input_args.extend(["--config", config_path_str])
+
     status_code = main(input_args)
     assert status_code == expected_status_code
 
