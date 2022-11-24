1772d88cc2a730a7deabb67538ccf86438c03ebd
dangkv <vo.dangkh@gmail.com>
2022-11-24 17:02:57 -0500

    chore: minor refactor on args

--- dbt_gloss/check_macro_arguments_have_desc.py
+++ dbt_gloss/check_macro_arguments_have_desc.py
@@ -9,10 +9,7 @@ from typing import Optional
 from typing import Sequence
 from typing import Set
 
-from dbt_gloss.utils import add_config_args
-from dbt_gloss.utils import add_filenames_args
-from dbt_gloss.utils import add_manifest_args
-from dbt_gloss.utils import add_tracking_args
+from dbt_gloss.utils import add_default_args
 from dbt_gloss.utils import get_filenames
 from dbt_gloss.utils import get_json
 from dbt_gloss.utils import get_macro_schemas
@@ -81,10 +78,7 @@ def check_argument_desc(
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
-    add_config_args(parser)
-    add_filenames_args(parser)
-    add_manifest_args(parser)
-    add_tracking_args(parser)
+    add_default_args(parser)
 
     args = parser.parse_args(argv)
 
@@ -99,7 +93,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     end_time = time.time()
     script_args = vars(args)
 
-    tracker = dbtGlossTracking()
+    tracker = dbtGlossTracking(script_args=script_args)
     tracker.track_hook_event(
         event_name="Hook Executed",
         manifest=manifest,
@@ -110,7 +104,6 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             "execution_time": end_time - start_time,
             "is_pytest": script_args.get("is_test"),
         },
-        script_args=script_args,
     )
 
     return hook_properties.get("status_code")
